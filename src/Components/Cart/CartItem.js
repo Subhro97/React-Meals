fb28da7d17b9f14e656aa675c9fb0e73032a0a90
Subhrojit Mondal <subhromond@gmail.com>
2021-09-06 23:26:22 +0530

    meals

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -2,6 +2,7 @@ import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
+  console.log("hi")
 
   return (
     <li className={classes['cart-item']}>
